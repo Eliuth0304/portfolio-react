ddfe95175aee7ff74cf86e4cc096022e6a71fb50
darkhorse930304 <darkhorse0304@gmail.com>
2023-03-27 18:05:17 -0400

    change some information

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
   email: "sjlim0802@gmail.com",
   phone: "+65 8874 8200",
   address: "Blk 349 Clementi Ave #0503 120349 Singapoe, Singapore",
-  github: "https://github.com/darkhorse930304",
+  github: "https://github.com/mrlim0802",
   linkedIn: "https://www.linkedin.com/in/lim-say-juan-569438269",
   devUsername: "Mr Lim",
 };
