48131de13993f4e7df799a4eb93fb488bee45716
darkhorse930304 <darkhorse0304@gmail.com>
2023-04-06 15:38:28 -0400

    upgrade phone number

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
   email: "sjlim0802@gmail.com",
-  phone: "+65 8874 8200",
+  phone: "+1 217 695 4778",
   address: "Blk 349 Clementi Ave #0503 120349 Singapoe, Singapore",
   github: "https://github.com/mrlim0802",
   linkedIn: "https://www.linkedin.com/in/lim-say-juan-569438269",
